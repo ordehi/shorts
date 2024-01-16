03a4761022ffadd41bdaec3790826b85d0657a5d
Joshua Ordehi <45109738+ordehi@users.noreply.github.com>
2024-01-15 20:52:45 -0400

    fix hiding bug

--- content.js
+++ content.js
@@ -1,27 +1,28 @@
 window.addEventListener('load', function () {
-  const hideShorts = (hide) => {
-    document.querySelectorAll('ytd-rich-shelf-renderer').forEach((element) => {
-      if (
-        element.querySelector('span#title') &&
-        element.querySelector('span#title').textContent.includes('Shorts')
-      ) {
-        element.style.display = hide ? 'none' : '';
-      }
+  const hideShorts = () => {
+    chrome.storage.sync.get('hideShorts', function (data) {
+      const hide = data.hideShorts;
+      document
+        .querySelectorAll('ytd-rich-shelf-renderer')
+        .forEach((element) => {
+          if (
+            element.querySelector('span#title') &&
+            element.querySelector('span#title').textContent.includes('Shorts')
+          ) {
+            element.style.display = hide ? 'none' : '';
+          }
+        });
     });
   };
 
-  chrome.storage.sync.get('hideShorts', function (data) {
-    if (data.hideShorts) {
-      hideShorts(data.hideShorts);
-    }
-  });
+  hideShorts();
 
   chrome.runtime.onMessage.addListener(function (
     request,
     sender,
     sendResponse
   ) {
-    hideShorts(request.hideShorts);
+    hideShorts();
   });
 
   const debounce = (func, delay) => {
